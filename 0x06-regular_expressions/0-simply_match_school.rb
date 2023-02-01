7854487c8493bc9fa7e72e6c8a4278237c7befbf
peter njihia <peternjihia2001@gmail.com>
2023-02-01 16:57:31 +0000

    task 0

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,2 +1,14 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/Holberton/).join
+
+# define the regular expression
+regex = /School/
+
+# get the input string from the command line argument
+input_string = ARGV[0]
+
+# Use the `=~` operator to match the input string against the regex
+if input_string =~ regex
+  puts "The input string '#{input_string}' matches the pattern 'School'"
+else
+  puts "The input string '#{input_string}' does not match the pattern 'School'"
+puts ARGV[0].scan(/School/).join

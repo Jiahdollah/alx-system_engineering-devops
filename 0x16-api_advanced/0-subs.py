ce5aa7b1b50a3f8b161d7a8cc1fc4ea9f3fb807c
peter njihia <peternjihia2001@gmail.com>
2023-05-09 21:11:09 +0000

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,16 @@
 #!/usr/bin/python3
+"""function that query subscribers on a given Reddit subreddit."""
 import requests
-import sys
 
 
 def number_of_subscribers(subreddit):
-    """ queries to Reddit API """
-    u_agent = 'Jiahdollah'
-
-    headers = {
-        'User-Agent': u_agent
-    }
-
+    """return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    res = requests.get(url, headers=headers, allow_redirects=False)
-    if res.status_code != 200:
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-    dic = res.json()
-    if 'data' not in dic:
-        return 0
-    if 'subscribers' not in dic.get('data'):
-        return 0
-    return res.json()['data']['subscribers']
+    results = response.json().get("data")
+    return results.get("subscribers")
---
+++ 0x16-api_advanced/tests/0-sub.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+function to query subscribers on a given Reddit subreddit
+"""
+import requests
+import sys
+
+
+def number_of_subscribers(subreddit):
+    """ queries to Reddit API """
+    u_agent = 'Mozilla/5.0'
+
+    headers = {
+        'User-Agent': u_agent
+    }
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    if res.status_code != 200:
+        return 0
+    dic = res.json()
+    if 'data' not in dic:
+        return 0
+    if 'subscribers' not in dic.get('data'):

42366ba78d32746b646cb64c950814c8fe63f0e4
peter njihia <peternjihia2001@gmail.com>
2023-08-08 06:26:02 -0700

    how many subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """on a given subreddit return total number of subscribers."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+            "user-agent: "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
